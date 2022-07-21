cc0e7944f620cc4caeda8c59dc41f854e581a985
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-07-21 17:56:50 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBar';
+import cloud from './cloud.png';
+import { useState } from 'react';
+
+
+
+function App() {
+
+const url=`https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=dd34722e1312392913bdae9cd8aaeca0`
+
+const api={
+  key:"dd34722e1312392913bdae9cd8aaeca0",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
+
+  return (
+    <>
+    <div className="container" style={{backgroundImage:`url(${cloud})`,height:'1000px',width:'2000px',backgroundSize:'cover',backgroundRepeat: 'no-repeat',position:"fixed"}}>
+    
+    <h1 className="display-1 text-center color-white $display-font-weight: 300">Weather</h1>
+
+    
+    </div>
+    
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SearchBar() {
+  return (
+    <div>
+        <div className="input-group mb-3">
+  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+  <button className="btn btn-outline-success" type="button" id="button-addon2">Button</button>
+</div>
+
+    </div>
+  )
+}
